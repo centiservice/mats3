2e00572daaf7eeff4280152f2ba4fac4ffe25e92
Endre Stølsvik <endre@stolsvik.com>
2023-04-13 22:08:18 +0200

    Minimal improvement to comments of test/example

--- mats-spring-test/src/test/java/io/mats3/spring/test/apptest1/AppEndpoint_MainService.java
+++ mats-spring-test/src/test/java/io/mats3/spring/test/apptest1/AppEndpoint_MainService.java
@@ -30,24 +30,27 @@ public class AppEndpoint_MainService {
 
         @Stage(Stage.INITIAL)
         void initialStage(SpringTestDataTO in) {
-            // Assert initial values of State
+            // Assert initial values of state (i.e. the state field _initialNumber)
             Assert.assertEquals(0, _initialNumber, 0);
 
-            // Set the initial number
+            // Set the initial number on the state, since we need it in the last stage
             _initialNumber = in.number;
 
+            // Perform request to the LEAFENDPOINT1, using the values we got from the request.
             _context.request(AppMain_MockAndTestingHarnesses.ENDPOINT_ID_LEAFENDPOINT1, new SpringTestDataTO(in.number,
                     in.string + ":(π=" + _calculator.multiplyByΠ(1) + ')'));
         }
 
         @Stage(10)
         void secondStage(SpringTestDataTO in) {
+            // Perform request to the LEAFENDPOINT2, using the values we got returned from LEAFENDPOINT2
             _context.request(AppMain_MockAndTestingHarnesses.ENDPOINT_ID_LEAFENDPOINT2, new SpringTestDataTO(in.number,
                     in.string + ":(φ=" + _calculator.multiplyByΦ(1) + ')'));
         }
 
         @Stage(20)
         SpringTestDataTO lastStage(SpringTestDataTO in) {
+            // Create reply, using values we got returned from LEAFENDPOINT2, plus the value we stored in state
             return new SpringTestDataTO(in.number * _initialNumber,
                     in.string + ':' + AppMain_MockAndTestingHarnesses.ENDPOINT_ID_MAINENDPOINT);
         }
