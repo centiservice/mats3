414fa8acb28a4d77aaabc24714793e071bfcb041
Endre Stølsvik <endre@stolsvik.com>
2023-05-02 14:11:24 +0200

    Version bump: 0.19.7-2023-04-16 -> 0.19.8-2023-05-02

--- mats-impl-jms/src/main/java/io/mats3/impl/jms/JmsMatsFactory.java
+++ mats-impl-jms/src/main/java/io/mats3/impl/jms/JmsMatsFactory.java
@@ -48,7 +48,7 @@ import io.mats3.serial.MatsTrace;
 public class JmsMatsFactory<Z> implements MatsInterceptableMatsFactory, JmsMatsStatics, JmsMatsStartStoppable {
 
     private static String MATS_IMPLEMENTATION_NAME = "JMS Mats";
-    private static String MATS_IMPLEMENTATION_VERSION = "0.19.7-2023-04-16";
+    private static String MATS_IMPLEMENTATION_VERSION = "0.19.8-2023-05-02";
 
     public static final String INTERCEPTOR_CLASS_MATS_LOGGING = "io.mats3.intercept.logging.MatsMetricsLoggingInterceptor";
     public static final String INTERCEPTOR_CLASS_MATS_METRICS = "io.mats3.intercept.micrometer.MatsMicrometerInterceptor";
