3f4be9f0aea57d4a966718d81855642fca147eb1
Endre Stølsvik <endre@stolsvik.com>
2023-05-15 09:10:36 +0200

    Testing: Cleaning and more thoroughly testing the "setInitiateTraceIdModifier(..)" concept.
    Yeah, have still not found any problem with it. Seems like the problem
    was on the API user side.

--- mats-api-test/src/test/java/io/mats3/api_test/basics/Test_InitiateTraceIdModifier.java
+++ mats-api-test/src/test/java/io/mats3/api_test/basics/Test_InitiateTraceIdModifier.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -25,12 +24,12 @@ import io.mats3.test.MatsTestLatch.Result;
 import io.mats3.test.junit.Rule_Mats;
 
 /**
- * Tests that the {@link io.mats3.MatsFactory.FactoryConfig#setInitiateTraceIdModifier(Function) Initiate
- * TraceId Modifier Function} works. There are also tests that checks that if the TraceId modifier employs the MDC,
- * we do not get a recursive situation when sending many messages in one initiation (this happened because when
- * setting the TraceId from within a initiation, the MDC is also set to the result. Thus, if you sent one more,
- * the modifier function would pick up the newly set traceId, and thus you got the result of "recursion" where
- * this "one more" message would get the previous message's traceId prepended, and not only the initial MDC).
+ * Tests that the {@link io.mats3.MatsFactory.FactoryConfig#setInitiateTraceIdModifier(Function) Initiate TraceId
+ * Modifier Function} works. There are also tests that checks that if the TraceId modifier employs the MDC, we do not
+ * get a recursive situation when sending many messages in one initiation (this happened because when setting the
+ * TraceId from within a initiation, the MDC is also set to the result. Thus, if you sent one more, the modifier
+ * function would pick up the newly set traceId, and thus you got the result of "recursion" where this "one more"
+ * message would get the previous message's traceId prepended, and not only the initial MDC).
  *
  * @author Endre Stølsvik 2021-04-12 21:15 - http://stolsvik.com/, endre@stolsvik.com
  */
@@ -40,34 +39,12 @@ public class Test_InitiateTraceIdModifier {
     @ClassRule
     public static final Rule_Mats MATS = Rule_Mats.create();
 
-    private static final String TERMINATOR = MatsTestHelp.terminator();
-    private static final String TERMINATOR_MANY = MatsTestHelp.endpoint("Terminator_Many");
-
-    // For the "many messages" situation:
-    private static volatile List<String> _traceIdsWithinMats;
-    private static volatile CountDownLatch _countDownLatch;
-
-    @BeforeClass
-    public static void setupTerminator() {
-        MATS.getMatsFactory().terminator(TERMINATOR, StateTO.class, DataTO.class,
-                (context, sto, dto) -> {
-                    log.debug("TERMINATOR MatsTrace:\n" + context.toString());
-                    MATS.getMatsTestLatch().resolve(context, sto, dto);
-                });
-
-        MATS.getMatsFactory().terminator(TERMINATOR_MANY, StateTO.class, DataTO.class,
-                (context, sto, dto) -> {
-                    _traceIdsWithinMats.add(context.getTraceId());
-                    _countDownLatch.countDown();
-                });
-    }
-
     @After
     @Before
-    public void clearMdc() {
+    public void clearMdcAndMatsFactories() {
         MDC.clear();
+        MATS.cleanMatsFactories();
     }
-
 
     /**
      * Dynamic TraceId modifier, using existing MDC to pick out traceId. This makes for a recursion problem, since the
@@ -78,7 +55,7 @@ public class Test_InitiateTraceIdModifier {
         MATS.getMatsFactory().getFactoryConfig().setInitiateTraceIdModifier((origTraceId) -> {
             String mdcTraceId = MDC.get("traceId");
             if (mdcTraceId != null) {
-                return mdcTraceId + "|" + origTraceId;
+                return mdcTraceId + "+" + origTraceId;
             }
             // E-> No MDC
             return origTraceId;
@@ -88,7 +65,14 @@ public class Test_InitiateTraceIdModifier {
     @Test
     public void simpleModifyTraceId() {
         // :: Arrange
-        MATS.getMatsFactory().getFactoryConfig().setInitiateTraceIdModifier((origTraceId) -> "Prefixed|" + origTraceId);
+        MATS.getMatsFactory().getFactoryConfig().setInitiateTraceIdModifier((origTraceId) -> "Prefixed+" + origTraceId);
+
+        String TERMINATOR = MatsTestHelp.terminator();
+        MATS.getMatsFactory().terminator(TERMINATOR, StateTO.class, DataTO.class,
+                (context, sto, dto) -> {
+                    log.debug("TERMINATOR MatsTrace:\n" + context.toString());
+                    MATS.getMatsTestLatch().resolve(context, sto, dto);
+                });
 
         // :: Act
         StateTO sto = new StateTO(7, 3.14);
@@ -105,54 +89,21 @@ public class Test_InitiateTraceIdModifier {
         Result<StateTO, DataTO> result = MATS.getMatsTestLatch().waitForResult();
         Assert.assertEquals(dto, result.getData());
         Assert.assertEquals(sto, result.getState());
-        Assert.assertEquals("Prefixed|" + origTraceId, result.getContext().getTraceId());
+        Assert.assertEquals("Prefixed+" + origTraceId, result.getContext().getTraceId());
     }
 
     @Test
-    public void withinStageShouldNotModify() {
-        // :: Arrange
-        MATS.getMatsFactory().getFactoryConfig().setInitiateTraceIdModifier((origTraceId) -> "Prefixed|" + origTraceId);
-
-        // :: Create a single-stage Terminator, which sends a new message (now with an already modified MDC)
-        String sendingTerminator = MatsTestHelp.endpoint();
-        MATS.getMatsFactory().terminator(sendingTerminator, StateTO.class, DataTO.class, (ctx, state, msg) -> {
-            ctx.initiate(init -> init.traceId("WithinStage").to(TERMINATOR).send(msg, state));
-        });
-
-        // :: Act
-        try {
-            StateTO sto = new StateTO(7, 3.14);
-            DataTO dto = new DataTO(42, "TheAnswer");
-            String origTraceId = MatsTestHelp.traceId();
-            MATS.getMatsInitiator().initiateUnchecked(
-                    (init) -> init
-                            .traceId(origTraceId)
-                            .from(MatsTestHelp.from("test"))
-                            .to(sendingTerminator)
-                            .send(dto, sto));
-
-            // :: Assert
-            Result<StateTO, DataTO> result = MATS.getMatsTestLatch().waitForResult();
-            Assert.assertEquals(dto, result.getData());
-            Assert.assertEquals(sto, result.getState());
-
-            // NOTE: The TraceId was prefixed with "Prefixed|" at the "from the outside" initiation, but NOT when
-            // initiated new message within Stage (otherwise, there would be 2x the prefix).
-            Assert.assertEquals("Prefixed|" + origTraceId + "|WithinStage", result.getContext().getTraceId());
-        }
-        finally {
-            // Remove the special endpoint we made
-            MATS.getMatsFactory().getEndpoint(sendingTerminator)
-                    .orElseThrow(() -> new AssertionError("Endpoint not present"))
-                    .remove(1000);
-        }
-    }
-
-    @Test
-    public void usingExistingMdcTraceIdToModify_set() {
+    public void usingExistingMdcTraceIdToModify_mdcSet() {
         // :: Arrange
         setTraceIdModifier_UsingExistingMdcTraceId();
         MDC.put("traceId", "Prefixed");
+
+        String TERMINATOR = MatsTestHelp.terminator();
+        MATS.getMatsFactory().terminator(TERMINATOR, StateTO.class, DataTO.class,
+                (context, sto, dto) -> {
+                    log.debug("TERMINATOR MatsTrace:\n" + context.toString());
+                    MATS.getMatsTestLatch().resolve(context, sto, dto);
+                });
 
         // :: Act
         StateTO sto = new StateTO(7, 3.14);
@@ -170,14 +121,21 @@ public class Test_InitiateTraceIdModifier {
         Assert.assertEquals(dto, result.getData());
         Assert.assertEquals(sto, result.getState());
         // Should have been modified (since MDC was set)
-        Assert.assertEquals("Prefixed|" + origTraceId, result.getContext().getTraceId());
+        Assert.assertEquals("Prefixed+" + origTraceId, result.getContext().getTraceId());
     }
 
     @Test
-    public void usingExistingMdcTraceIdToModify_null() {
+    public void usingExistingMdcTraceIdToModify_mdcNull() {
         // :: Arrange
         setTraceIdModifier_UsingExistingMdcTraceId();
         // !Not setting MDC.traceId
+
+        String TERMINATOR = MatsTestHelp.terminator();
+        MATS.getMatsFactory().terminator(TERMINATOR, StateTO.class, DataTO.class,
+                (context, sto, dto) -> {
+                    log.debug("TERMINATOR MatsTrace:\n" + context.toString());
+                    MATS.getMatsTestLatch().resolve(context, sto, dto);
+                });
 
         // :: Act
         StateTO sto = new StateTO(7, 3.14);
@@ -204,9 +162,17 @@ public class Test_InitiateTraceIdModifier {
         setTraceIdModifier_UsingExistingMdcTraceId();
         MDC.put("traceId", "Prefixed");
 
+        String TERMINATOR_MANY = MatsTestHelp.endpoint("Terminator_Many");
+
         int numMessages = 20;
-        _countDownLatch = new CountDownLatch(20);
-        _traceIdsWithinMats = new CopyOnWriteArrayList<>();
+        CountDownLatch countDownLatch = new CountDownLatch(numMessages);
+        List<String> traceIdsWithinMats = new CopyOnWriteArrayList<>();
+
+        MATS.getMatsFactory().terminator(TERMINATOR_MANY, StateTO.class, DataTO.class,
+                (context, sto, dto) -> {
+                    traceIdsWithinMats.add(context.getTraceId());
+                    countDownLatch.countDown();
+                });
 
         // :: Act
         StateTO sto = new StateTO(7, 3.14);
@@ -225,19 +191,19 @@ public class Test_InitiateTraceIdModifier {
                 });
 
         // :: Assert
-        boolean waited = _countDownLatch.await(10, TimeUnit.SECONDS);
+        boolean waited = countDownLatch.await(10, TimeUnit.SECONDS);
         Assert.assertTrue("Should have counted down!", waited);
 
         // Assert that we have all the traceIds
-        Assert.assertEquals(_traceIdsWithinMats.size(), numMessages);
-        _traceIdsWithinMats.sort(Comparator.naturalOrder());
-        log.info("######### All traceIds within Mats: " + _traceIdsWithinMats);
+        Assert.assertEquals(traceIdsWithinMats.size(), numMessages);
+        traceIdsWithinMats.sort(Comparator.naturalOrder());
+        log.info("######### All traceIds within Mats: " + traceIdsWithinMats);
         // Should have been modified (since MDC was set)
         List<String> expectedTraceIds = origTraceIds.stream()
-                .map(s -> "Prefixed|" + s)
+                .map(s -> "Prefixed+" + s)
                 .sorted()
                 .collect(Collectors.toList());
-        Assert.assertEquals(expectedTraceIds, _traceIdsWithinMats);
+        Assert.assertEquals(expectedTraceIds, traceIdsWithinMats);
     }
 
     @Test
@@ -246,9 +212,17 @@ public class Test_InitiateTraceIdModifier {
         setTraceIdModifier_UsingExistingMdcTraceId();
         // !Not setting MDC.traceId
 
+        String TERMINATOR_MANY = MatsTestHelp.endpoint("Terminator_Many");
+
         int numMessages = 20;
-        _countDownLatch = new CountDownLatch(20);
-        _traceIdsWithinMats = new CopyOnWriteArrayList<>();
+        CountDownLatch countDownLatch = new CountDownLatch(numMessages);
+        List<String> traceIdsWithinMats = new CopyOnWriteArrayList<>();
+
+        MATS.getMatsFactory().terminator(TERMINATOR_MANY, StateTO.class, DataTO.class,
+                (context, sto, dto) -> {
+                    traceIdsWithinMats.add(context.getTraceId());
+                    countDownLatch.countDown();
+                });
 
         // :: Act
         StateTO sto = new StateTO(7, 3.14);
@@ -267,18 +241,128 @@ public class Test_InitiateTraceIdModifier {
                 });
 
         // :: Assert
-        boolean waited = _countDownLatch.await(10, TimeUnit.SECONDS);
+        boolean waited = countDownLatch.await(10, TimeUnit.SECONDS);
         Assert.assertTrue("Should have counted down!", waited);
 
-
         // Assert that we have all the traceIds
-        Assert.assertEquals(_traceIdsWithinMats.size(), numMessages);
-        _traceIdsWithinMats.sort(Comparator.naturalOrder());
-        log.info("######### All traceIds within Mats: " + _traceIdsWithinMats);
+        Assert.assertEquals(traceIdsWithinMats.size(), numMessages);
+        traceIdsWithinMats.sort(Comparator.naturalOrder());
+        log.info("######### All traceIds within Mats: " + traceIdsWithinMats);
         // Should NOT have been modified (since MDC was null)
         List<String> expectedTraceIds = origTraceIds.stream()
                 .sorted()
                 .collect(Collectors.toList());
-        Assert.assertEquals(expectedTraceIds, _traceIdsWithinMats);
+        Assert.assertEquals(expectedTraceIds, traceIdsWithinMats);
+    }
+
+    @Test
+    public void usingExistingMdcTraceIdToModify_initiateWithStageShouldNotModify() {
+        // :: Arrange
+        setTraceIdModifier_UsingExistingMdcTraceId();
+        MDC.put("traceId", "Prefixed");
+
+        String TERMINATOR = MatsTestHelp.terminator();
+        MATS.getMatsFactory().terminator(TERMINATOR, StateTO.class, DataTO.class,
+                (context, sto, dto) -> {
+                    log.debug("TERMINATOR MatsTrace:\n" + context.toString());
+                    MATS.getMatsTestLatch().resolve(context, sto, dto);
+                });
+
+        // :: Create a single-stage Terminator, which sends a new message (now with an already modified MDC)
+        String sendingTerminator = MatsTestHelp.endpoint();
+        MATS.getMatsFactory().terminator(sendingTerminator, StateTO.class, DataTO.class, (ctx, state, msg) -> {
+            ctx.initiate(init -> init.traceId("WithinStage").to(TERMINATOR).send(msg, state));
+        });
+
+        // :: Act
+        StateTO sto = new StateTO(7, 3.14);
+        DataTO dto = new DataTO(42, "TheAnswer");
+        String origTraceId = MatsTestHelp.traceId();
+        MATS.getMatsInitiator().initiateUnchecked(
+                (init) -> init
+                        .traceId(origTraceId)
+                        .from(MatsTestHelp.from("test"))
+                        .to(sendingTerminator)
+                        .send(dto, sto));
+
+        // :: Assert
+        Result<StateTO, DataTO> result = MATS.getMatsTestLatch().waitForResult();
+        Assert.assertEquals(dto, result.getData());
+        Assert.assertEquals(sto, result.getState());
+
+        // NOTE: The TraceId was prefixed with "Prefixed+" at the "from the outside" initiation, but NOT when
+        // initiated new message within Stage (otherwise, there would be 2x the prefix).
+        Assert.assertEquals("Prefixed+" + origTraceId + "|WithinStage", result.getContext().getTraceId());
+    }
+
+    @Test
+    public void usingExistingMdcTraceIdToModify_initiateWithStageShouldNotModify_manyMessages()
+            throws InterruptedException {
+        // :: Arrange
+        setTraceIdModifier_UsingExistingMdcTraceId();
+        MDC.put("traceId", "Prefixed");
+
+        int numMessages = 5;
+        CountDownLatch countDownLatch = new CountDownLatch(numMessages * 2);
+        List<String> traceIdsWithinMats = new CopyOnWriteArrayList<>();
+
+        String TERMINATOR = MatsTestHelp.terminator();
+        MATS.getMatsFactory().terminator(TERMINATOR, StateTO.class, DataTO.class,
+                (context, sto, dto) -> {
+                    traceIdsWithinMats.add(context.getTraceId());
+                    countDownLatch.countDown();
+                });
+
+        // :: Create a single-stage Terminator, which sends a new message (now with an already modified MDC)
+        String sendingTerminator = MatsTestHelp.endpoint();
+        MATS.getMatsFactory().terminator(sendingTerminator, StateTO.class, DataTO.class, (ctx, state, msg) -> {
+            // First, repeatedly outside the InitiateLambda
+            for (int i = 0; i < numMessages; i++) {
+                final int iF = i;
+                ctx.initiate(init -> init.traceId("WithinStage_RepOutsideInitiate#" + iF)
+                        .to(TERMINATOR).send(msg, state));
+            }
+
+            // Then, repeatedly inside the InitiateLambda
+            ctx.initiate(init -> {
+                for (int i = 0; i < numMessages; i++) {
+                    init.traceId("WithinStage_RepWithinInitiate#" + i)
+                            .to(TERMINATOR).send(msg, state);
+                }
+            });
+        });
+
+        // :: Act
+        StateTO sto = new StateTO(7, 3.14);
+        DataTO dto = new DataTO(42, "TheAnswer");
+        String origTraceId = MatsTestHelp.traceId();
+        MATS.getMatsInitiator().initiateUnchecked(
+                (init) -> init
+                        .traceId(origTraceId)
+                        .from(MatsTestHelp.from("test"))
+                        .to(sendingTerminator)
+                        .send(dto, sto));
+
+        // :: Assert
+        boolean waited = countDownLatch.await(10, TimeUnit.SECONDS);
+        Assert.assertTrue("Should have counted down!", waited);
+
+        log.info("######### All traceIds within Mats: " + traceIdsWithinMats);
+
+        // Construct the expected set of traceIds
+        List<String> expectedTraceIds = new ArrayList<>();
+        for (int i = 0; i < numMessages; i++) {
+            expectedTraceIds.add("Prefixed+" + origTraceId + "|WithinStage_RepOutsideInitiate#" + i);
+            expectedTraceIds.add("Prefixed+" + origTraceId + "|WithinStage_RepWithinInitiate#" + i);
+        }
+        expectedTraceIds.sort(Comparator.naturalOrder());
+
+        // Assert that we have all the traceIds
+        Assert.assertEquals(traceIdsWithinMats.size(), numMessages * 2);
+        traceIdsWithinMats.sort(Comparator.naturalOrder());
+
+        // NOTE: The TraceId was prefixed with "Prefixed+" at the "from the outside" initiation, but NOT when
+        // initiated new message within Stage (otherwise, there would be 2x the prefix).
+        Assert.assertEquals(expectedTraceIds, traceIdsWithinMats);
     }
 }
