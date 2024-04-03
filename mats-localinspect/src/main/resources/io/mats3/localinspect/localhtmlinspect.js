05b242fafa172f86dac5840447ab8272cf22901f
Endre Stølsvik <endre@stolsvik.com>
2024-04-03 20:39:26 +0200

    Fixing tooltip positioning for "localinspect", which led to annoying scrollbar.
    The toolbar used space "outside" the window, and thus introduced a
    scrollbar. This also of course made the tooltip impossible to read
    fully, since it was offscreen. 4 hours later, the tooltip now
    dynamically repositions, including the caret. Thanks ChatGPT!

--- mats-localinspect/src/main/resources/io/mats3/localinspect/localhtmlinspect.js
+++ mats-localinspect/src/main/resources/io/mats3/localinspect/localhtmlinspect.js
@@ -7,6 +7,7 @@ function matsli_messages_summary(event) {
     parentSpan.querySelector(".matsli_msgs_summary_btn").classList.add("matsli_msgs_summary_or_details_btn_active");
 
 }
+
 function matsli_messages_details(event) {
     let button = event.target;
     let parentSpan = button.closest("span")
@@ -14,4 +15,60 @@ function matsli_messages_details(event) {
     parentSpan.querySelector(".matsli_msgs_details_btn").classList.add("matsli_msgs_summary_or_details_btn_active");
     parentSpan.querySelector(".matsli_msgs_summary").classList.add("matsli_noshow");
     parentSpan.querySelector(".matsli_msgs_summary_btn").classList.remove("matsli_msgs_summary_or_details_btn_active");
+}
+
+// ::: CODE TO BE RUN AFTER DOMContentLoaded and on resize :::
+
+function matsli_debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+const matsli_debounced_reposition_tooltip = matsli_debounce(() => {
+    document.querySelectorAll('.matsli_tooltip').forEach(matsli_reposition_tooltip);
+}, 100);
+
+document.addEventListener('DOMContentLoaded', matsli_debounced_reposition_tooltip);
+window.addEventListener('resize', matsli_debounced_reposition_tooltip);
+
+function matsli_reposition_tooltip(tooltip) {
+    let tooltip_Rect = tooltip.getBoundingClientRect();
+    let tooltiptext = tooltip.querySelector('.matsli_tooltiptext');
+    let tooltiptext_Rect = tooltiptext.getBoundingClientRect();
+
+    // How much space to the left and right of the hover element
+    let spaceLeft = tooltip_Rect.left;
+    let spaceRight = window.innerWidth - tooltip_Rect.right;
+    let tooltiptext_Width = tooltiptext_Rect.width;
+
+    const offset = 18;
+
+    let tooltipLeft = 'auto'
+    let tooltipRight = 'auto';
+    let caretLeft = '50%';
+
+    // Reposition tooltip and caret based on available space
+    if ((spaceRight - offset) < tooltiptext_Width / 2) {
+        // Not enough space on the right, so we use as much space as possible on the right
+        tooltipRight = (-spaceRight) + offset + 'px';
+        caretLeft = tooltiptext_Width - spaceRight + (tooltip_Rect.width / 2) + "px";
+    } else if (spaceLeft < tooltiptext_Width / 2) {
+        // Not enough space on the left, so we use as much space as possible on the left
+        tooltipLeft = (-spaceLeft) + offset + 'px';
+        caretLeft = spaceLeft - offset + (tooltip_Rect.width / 2) + "px";
+    } else {
+        // Enough space on both sides, so we center it - relative to the hover element
+        tooltipLeft = -(tooltiptext_Width / 2) + (tooltip_Rect.width / 2) + 'px';
+        caretLeft = '50%';
+    }
+    tooltiptext.style.left = tooltipLeft;
+    tooltiptext.style.right = tooltipRight;
+    tooltiptext.style.setProperty('--caret-left', caretLeft);
 }
