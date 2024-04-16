e94cba1ae238a6238fca6eb9176496a6fb8b0e01
Endre Stølsvik <endre@stolsvik.com>
2024-04-16 18:37:45 +0200

    Version bump: 0.19.16-2024-04-16 -> 0.19.17-2024-04-16
    Quick version to get out a small feature; CommonEndpointGroupId

--- mats-impl-jms/src/main/java/io/mats3/impl/jms/JmsMatsFactory.java
+++ mats-impl-jms/src/main/java/io/mats3/impl/jms/JmsMatsFactory.java
@@ -49,7 +49,7 @@ import io.mats3.serial.MatsTrace;
 public class JmsMatsFactory<Z> implements JmsMatsStatics, JmsMatsStartStoppable, MatsFactory {
 
     private static final String MATS_IMPLEMENTATION_NAME = "JMS Mats3";
-    private static final String MATS_IMPLEMENTATION_VERSION = "0.19.16-2024-04-16";
+    private static final String MATS_IMPLEMENTATION_VERSION = "0.19.17-2024-04-16";
 
     public static final String INTERCEPTOR_CLASS_MATS_LOGGING = "io.mats3.intercept.logging.MatsMetricsLoggingInterceptor";
     public static final String INTERCEPTOR_CLASS_MATS_METRICS = "io.mats3.intercept.micrometer.MatsMicrometerInterceptor";
