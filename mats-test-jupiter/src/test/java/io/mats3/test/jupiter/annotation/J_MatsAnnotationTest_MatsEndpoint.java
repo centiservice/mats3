200268e8c3ba3a919c8d31bb523e555f186d897c
Ståle Undheim <staale@staale.org>
2025-02-06 22:30:44 +0100

    Add support in Jupiter to configure via Annotations
    This adds the @MatsTest annotation, as well as annotations for creating
    test endpoints, and to register annotated classes. This is an
    alternative way to configure tests in Jupiter for Mats.
    
    By adding @MatsTest to a Jupiter test class, you can automatically
    inject MatsFactory and other Mats related instances into your test
    methods and constructors. The extension for the annotation will take
    care of registering Extension_Mats and providing these.
    
    You can also add Extension_MatsEndpoint fields witout initializing them,
    and instead annotating with @MatsTest.Endpoint to have the extension
    automatically create these fields. Thus avoiding having to repeat the
    types in the field as class parameters to the create method.
    
    Fields that have types with @MatsClassMapping or @MatsMapping can also
    be registered automatically by adding the annotation
    @MatsTest.AnnotatedClass. Combined with Mockito, this can make for a
    pleasent way to set up integration tests with Mats.

---
+++ mats-test-jupiter/src/test/java/io/mats3/test/jupiter/annotation/J_MatsAnnotationTest_MatsEndpoint.java
@@ -0,0 +1,58 @@
+package io.mats3.test.jupiter.annotation;
+
+import static io.mats3.test.jupiter.annotation.J_MatsAnnotationTest_MatsAnnotatedClass.callMatsAnnotatedEndpoint;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import io.mats3.test.jupiter.Extension_MatsEndpoint;
+import io.mats3.util.MatsFuturizer;
+
+/**
+ * Test to demonstrate how to use the {@link MatsTest.Endpoint} annotation to create a MatsEndpoint for testing.
+ *
+ * @author Ståle Undheim <stale.undheim@storebrand.no> 2025-02-06
+ */
+@MatsTest
+class J_MatsAnnotationTest_MatsEndpoint {
+
+    private static final String ENDPOINT_ID = "TestEndpoint";
+    private static final String NESTED_ENDPOINT_ID = "NestedTestEndpoint";
+
+    @MatsTest.Endpoint(endpointId = ENDPOINT_ID)
+    private Extension_MatsEndpoint<String, String> _matsEndpoint;
+
+    @Test
+    void testMatsEndpointRegistered(MatsFuturizer matsFuturizer)
+            throws ExecutionException, InterruptedException, TimeoutException {
+        _matsEndpoint.setProcessLambda((ctx, msg) -> "Hello " + msg);
+
+        String result = callMatsAnnotatedEndpoint(matsFuturizer, ENDPOINT_ID, "World");
+
+        Assertions.assertEquals("Hello World", result);
+    }
+
+    @Nested
+    class NestedEndpointRegistrationTest {
+
+        @MatsTest.Endpoint(endpointId = NESTED_ENDPOINT_ID)
+        private Extension_MatsEndpoint<String, String> _nestedEndpoint;
+
+        @Test
+        void testMatsEndpointRegistered(MatsFuturizer matsFuturizer)
+                throws ExecutionException, InterruptedException, TimeoutException {
+            _matsEndpoint.setProcessLambda((ctx, msg) -> "Outer message: " + msg);
+            _nestedEndpoint.setProcessLambda((ctx, msg) -> "Nested message: " + msg);
+
+            String resultOuter = callMatsAnnotatedEndpoint(matsFuturizer, ENDPOINT_ID, "Outer");
+            String resultNested = callMatsAnnotatedEndpoint(matsFuturizer, NESTED_ENDPOINT_ID, "Inner");
+
+            Assertions.assertEquals("Outer message: Outer", resultOuter);
+            Assertions.assertEquals("Nested message: Inner", resultNested);
+        }
+    }
+}
