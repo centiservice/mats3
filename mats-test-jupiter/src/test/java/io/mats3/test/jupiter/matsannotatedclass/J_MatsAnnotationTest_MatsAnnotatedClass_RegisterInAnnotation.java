bdf4b6fd2f0ace671657e964849c2db7c20857bb
Ståle Undheim <staale@staale.org>
2025-02-11 12:58:43 +0100

    Support registering endpoints in @MatsTest annotation
    This adds support for declaring classes that one wishes to be registered
    as endpoints on the MatsTest annotation itself. Thus avoiding the need
    to create a separate field for the annotated class, when it is not
    needed by the test.
    
    I contribute this material in accordance with the signed SCA.

---
+++ mats-test-jupiter/src/test/java/io/mats3/test/jupiter/matsannotatedclass/J_MatsAnnotationTest_MatsAnnotatedClass_RegisterInAnnotation.java
@@ -0,0 +1,86 @@
+package io.mats3.test.jupiter.matsannotatedclass;
+
+
+import static io.mats3.test.jupiter.matsannotatedclass.J_ExtensionMatsAnnotatedClass_BasicsAndNestingTest.callMatsAnnotatedEndpoint;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.mats3.spring.Dto;
+import io.mats3.spring.MatsMapping;
+import io.mats3.test.jupiter.MatsTest;
+import io.mats3.test.jupiter.matsannotatedclass.J_MatsAnnotationTest_MatsAnnotatedClass_RegisterInAnnotation.AnnotatedMats3Endpoint;
+import io.mats3.test.jupiter.matsannotatedclass.J_MatsAnnotationTest_MatsAnnotatedClass_RegisterInAnnotation.AnnotatedMats3Endpoint.ServiceDependency;
+import io.mats3.util.MatsFuturizer;
+
+/**
+ * Tests classes registered via {@link MatsTest#matsAnnotatedClasses()}.
+ *
+ * @author Ståle Undheim <stale.undheim@storebrand.no> 2025-02-11
+ */
+@ExtendWith(MockitoExtension.class)
+@MatsTest(matsAnnotatedClasses = AnnotatedMats3Endpoint.class)
+public class J_MatsAnnotationTest_MatsAnnotatedClass_RegisterInAnnotation {
+
+    // Provide the Service dependency for AnnotatedMats3Endpoint
+    @Mock
+    private ServiceDependency _serviceDependency;
+
+    @Test
+    void testAnnotatedMatsClass(MatsFuturizer futurizer) throws ExecutionException, InterruptedException, TimeoutException {
+        // :: Setup
+        String expectedReturn = "Hello World!";
+        when(_serviceDependency.formatMessage("World!")).thenReturn(expectedReturn);
+
+        // :: Act
+        String reply = callMatsAnnotatedEndpoint(futurizer, "World!");
+
+        // :: Verify
+        Assertions.assertEquals(expectedReturn, reply);
+        verify(_serviceDependency).formatMessage("World!");
+    }
+
+    // ---- Test classes, in normal code this would not be part of the test, but rather in your main code base ---------
+
+    /**
+     * Example of a class with annotated MatsEndpoints.
+     */
+    public static class AnnotatedMats3Endpoint {
+        private ServiceDependency _serviceDependency;
+
+        public static final String ENDPOINT_ID = "AnnotatedEndpoint";
+
+        /**
+         * Dummy example of a service dependency.
+         */
+        public interface ServiceDependency {
+            String formatMessage(String msg);
+        }
+
+        public AnnotatedMats3Endpoint() {
+            /* No-args constructor for Jackson deserialization. */
+        }
+
+        @Inject
+        public AnnotatedMats3Endpoint(ServiceDependency serviceDependency) {
+            _serviceDependency = serviceDependency;
+        }
+
+        @MatsMapping(ENDPOINT_ID)
+        public String matsEndpoint(@Dto String msg) {
+            return _serviceDependency.formatMessage(msg);
+        }
+
+    }
+
+}
